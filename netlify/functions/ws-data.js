ff9511da9c5a6650eeb0e9d0aed5ca874d5c8df7
seidkarlinn <seidkarlinn@seidkarlinn.is>
2026-04-14 13:02:52 +0000

    fix: whitelist ws_product_info and ws_theme in ws-data

--- netlify/functions/ws-data.js
+++ netlify/functions/ws-data.js
@@ -2,7 +2,7 @@ const { getStore } = require("@netlify/blobs");
 
 const ALLOWED_KEYS = [
   "ws_orders","ws_invoice_seq","ws_buyer_accounts","ws_custom_products",
-  "ws_deleted_products","ws_pricing","ws_pricing_users","ws_vidskm","ws_last_order","ws_cache_version",
+  "ws_deleted_products","ws_pricing","ws_pricing_users","ws_vidskm","ws_last_order","ws_cache_version","ws_product_info","ws_theme",
 ];
 
 exports.handler = async (event) => {
